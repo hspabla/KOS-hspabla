dd3d290c53860ccae790f2dbf4b9a0151df52272
Martin Karsten <mkarsten@uwaterloo.ca>
2015-08-13 22:14:18 -0400

    - make memoryhog more aggressive and multithreaded

--- src/user/memoryhog.cc
+++ src/user/memoryhog.cc
@@ -15,14 +15,30 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ******************************************************************************/
 #include "syscalls.h"
+#include "pthread.h"
 
 #include <cstdio>
 #include <cstring>
 
+static pthread_mutex_t iolock;
+
+static void* task(void* x) {
+  void* p = malloc(1023*1023);
+  pthread_mutex_lock(&iolock);
+  printf("%p\n",p);
+  pthread_mutex_unlock(&iolock);
+  memset(p, 0, 1023*1023);
+}
+
 int main() {
+  pthread_mutex_init( &iolock, nullptr );
+  pthread_t t;
+  pthread_create(&t, nullptr, task, nullptr);
   while (1) {
     void* p = malloc(1023*1023);
+    pthread_mutex_lock(&iolock);
     printf("%p\n",p);
+    pthread_mutex_unlock(&iolock);
     memset(p, 0, 1023*1023);
   }
   return 0;
