b9cbc4c0db1641b7cce4a3e28b42ead982b095ba
Harsimrandeep Singh Pabla <hspabla@uwaterloo.ca>
2016-04-14 00:46:58 -0400

    fixed indentation

--- src/tools/ringbuf.h
+++ src/tools/ringbuf.h
@@ -11,18 +11,16 @@ using namespace std;
 class Queue{
     private:
         int head, tail, size, count = 0;
-				//sampleD sampleData;
-        //int *ringArray;
-				sampleD ringArray[QUEUE_SIZE];
+		sampleD ringArray[QUEUE_SIZE];
 	
     public:
         Queue();
-				~Queue();
+		~Queue();
         void push(sampleD);
         void print();
-				int qsize();
-				uint64_t view(int);
-				void data_read();
+		int qsize();
+		uint64_t view(int);
+		void data_read();
 };
 
 
